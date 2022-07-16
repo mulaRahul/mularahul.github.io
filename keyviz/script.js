8ca79b4f98ff04834406f1ba8ddba6b98f314bf8
mulaRahul <rahulmula10@gmail.com>
2022-07-16 15:25:58 +0530

    Replaced <div> lottie animations with <lottie-player>

--- keyviz/script.js
+++ keyviz/script.js
@@ -1,65 +1,41 @@
-const heroContainer = document.getElementById("hero-mockup");
-const pressContainer = document.getElementById("press-feedback");
-const reelContainer = document.getElementById("reel");
-const settingsWindowContainer = document.getElementById("settings-window");
 // lottie animations
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("LOADED!");
+const heroAnimation = document.querySelector("section.hero lottie-player");
+const pressAnimation = document.querySelector("section.press lottie-player");
+const reelAnimation = document.querySelector("section.style lottie-player");
+const settingsAnimation = document.querySelector("section.settings lottie-player");
+let heroPlayed = false;
+// play hero animation when it's ready
+heroAnimation.addEventListener("ready", () => {
+  heroAnimation.style.opacity = 1;
+  setTimeout(() => {
+    heroAnimation.play();
+    heroPlayed = true;
+  }, 1200);
 });
-const heroAnimation = bodymovin.loadAnimation({
-  container: heroContainer,
-  autoplay: false,
-  loop: false,
-  path: "keyviz/hero.json",
-});
-const pressAnimation = bodymovin.loadAnimation({
-  container: pressContainer,
-  renderer: "svg",
-  autoplay: false,
-  loop: true,
-  path: "keyviz/press.json",
-});
-const reelAnimation = bodymovin.loadAnimation({
-  container: reelContainer,
-  renderer: "svg",
-  autoplay: false,
-  loop: true,
-  path: "keyviz/reel.json",
-});
-const settingsAnimation = bodymovin.loadAnimation({
-  container: settingsWindowContainer,
-  renderer: "svg",
-  autoplay: false,
-  loop: true,
-  path: "keyviz/settings.json",
-});
-setTimeout(()=>heroAnimation.play(), 1600);
 // scroll based animations
 ScrollOut({
   onShown: function (element) {
-    if (element.id == "style-h1") {
-      reelContainer.style.opacity = 1;
+    if (element.id == "hero-h1" && heroPlayed) {
+      heroAnimation.play();
+    } else if (element.id == "press-h1") {
+      setTimeout(()=>pressAnimation.play(), 1000);
+    } else if (element.id == "style-h1") {
+      reelAnimation.style.opacity = 1;
       reelAnimation.play();
     } else if (element.id == "settings-h1") {
       settingsAnimation.play();
-      settingsWindowContainer.style.bottom = "-10%";
-    } else if (element.id == "hero-mockup") {
-      heroAnimation.play();
-    } else if (element.id == "press-feedback") {
-      pressAnimation.play();
     } 
   },
   onHidden: function (element) {
-    if (element.id == "style-h1") {
-      reelContainer.style.opacity = 0;
+    if (element.id == "hero-h1") {
+      heroAnimation.stop();
+    } else if (element.id == "press-h1") {
+      pressAnimation.pause();
+    } else if (element.id == "style-h1") {
+      reelAnimation.style.opacity = 0;
       reelAnimation.pause();
     } else if (element.id == "settings-h1") {
-      settingsWindowContainer.style.bottom = "0%";
       settingsAnimation.pause();
-    } else if (element.id == "hero-mockup") {
-      heroAnimation.stop();
-    } else if (element.id == "press-feedback") {
-      pressAnimation.pause();
     } 
   }
 });
